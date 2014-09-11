6f642dc8011b39d734f5e5004c1a9c6c15bc46cd
subeenjeon <powersubeen@naver.com>
2014-09-11 15:11:00 +0900

    d

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(640, 480);
+  background(128, 0, 64);
+  noFill();
+
+  stroke(255, 0, 0);
+
+  var x;
+
+  x = 100;
+  ellipse(x, 100, 50, 50);
+  x = 250;
+  ellipse(x, 100, 50, 50);
+  x = 300;
+  ellipse(x, 100, 50, 50);
+}
