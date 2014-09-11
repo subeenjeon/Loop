48ced107e1bb5aac1f1818436b477ed065629d73
subeenjeon <powersubeen@naver.com>
2014-09-11 15:12:20 +0900

    d

--- sketch.js
+++ sketch.js
@@ -3,14 +3,14 @@ function setup() {
   background(128, 0, 64);
   noFill();
 
-  stroke(255, 0, 0);
+  stroke(0, 111, 0);
 
   var x;
 
   x = 100;
   ellipse(x, 100, 50, 50);
   x = 250;
-  ellipse(x, 100, 50, 50);
+  ellipse(x, 200, 50, 50);
   x = 300;
   ellipse(x, 100, 50, 50);
 }
